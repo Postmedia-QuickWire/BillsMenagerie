87cb759cb70c586c0fdbbf875e491d8b9a6f5937
QuickWire <bmiller@postmedia.com>
2024-12-04 11:29:43 -0500

    dt models

---
+++ src/Models/DataTablesAjaxModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+
+	public class DataTablesAjaxSearch
+	{
+		public string value { get; set; }
+		public bool regex { get; set; }
+	}
+
+	public class DataTablesAjaxOrder
+	{
+		public int column { get; set; }
+		public string dir { get; set; }
+	}
+
+	public class DataTablesAjaxColumn
+	{
+		public int data { get; set; }
+		public string name { get; set; }
+		public bool searchable { get; set; }
+		public bool orderable { get; set; }
+		public DataTablesAjaxSearch search { get; set; }
+	}
+
+	public class DataTablesAjaxModel
+	{
+		/// <summary>
+		/// Draw counter. 
+		/// </summary>       
+		public int draw { get; set; }
+
+		/// <summary>
+		/// Paging first record indicator. This is the start point in the current data set  
+		/// </summary>       
+		public int start { get; set; }
+
+		/// <summary>
+		/// Number of records that the table can display in the current draw.  
+		/// </summary>       
+		public int length { get; set; }
+
+		public List<DataTablesAjaxColumn> columns { get; set; }
+		public List<DataTablesAjaxOrder> order { get; set; }
+
+		public DataTablesAjaxSearch search { get; set; }
+	}
+}
